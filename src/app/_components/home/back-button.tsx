5f34b00235766fa6e26f8de7e642b4c9f81435eb
Azo <1353761785@qq.com>
2025-10-09 20:53:48 +0800

    com-上一页按钮

--- src/app/_components/home/back-button.tsx
+++ src/app/_components/home/back-button.tsx
@@ -1,43 +1,37 @@
-/* eslint-disable react-hooks-extra/no-direct-set-state-in-use-effect */
-'use client';
-
 import type { FC, MouseEventHandler } from 'react';
-
 import { Undo2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useCallback, useEffect, useState } from 'react';
-
 import { Button } from '../shadcn/ui/button';
+import { useCallback, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { cn } from '../shadcn/utils';
 
 export const BackButton: FC = () => {
     const router = useRouter();
+
     const [historyLength, setHistoryLength] = useState(0);
 
     useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setHistoryLength(window.history.length);
+        if (typeof window != 'undefined') {
+            setHistoryLength(window.history?.length);
         }
     }, []);
-    const goBack: MouseEventHandler<HTMLButtonElement> = useCallback(
-        (e) => {
-            e.preventDefault();
-            if (historyLength > 1) router.back();
-        },
-        [historyLength],
-    );
+
+    const goBack: MouseEventHandler<HTMLButtonElement> = useCallback((e) => {
+        e.preventDefault();
+
+        if (typeof window != 'undefined' && historyLength > 1) {
+            router.back();
+        }else{
+            router.replace('/')
+        }
+    },[historyLength]);
 
     return (
-        <Button
-            variant="outline"
-            className={cn('rounded-sm', {
-                'pointer-events-none opacity-50': historyLength <= 1,
-            })}
-            disabled={historyLength <= 1}
-            aria-disabled={historyLength <= 1}
-            onClick={goBack}
-        >
+        <Button variant="outline" className={
+            cn('rounded-sm',{'pointer-events-auto opacity-50':historyLength<=1})
+        } onClick={goBack}>
             <Undo2 />
+            {historyLength}
             返回
         </Button>
     );
